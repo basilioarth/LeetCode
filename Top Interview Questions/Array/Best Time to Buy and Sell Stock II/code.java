75e8bf4f3a79c0cfc3fc26e5fe6e1a288bd721c9
basilioarth <basilio.arth@gmail.com>
2021-09-17 22:48:11 -0300

    Best Time to Buy and Sell Stock II | Solution by Brute Force

---
+++ Top Interview Questions/Array/Best Time to Buy and Sell Stock II/code.java
@@ -0,0 +1,30 @@
+/**
+ * Approach 1: Brute Force
+ * In this case, we simply calculate the profit corresponding to all the possible sets of transactions and find out the maximum profit out of them.
+ * Submission Result: Time Limit Exceeded 
+ */
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        return calculate(prices, 0);
+    }
+
+    public int calculate(int prices[], int s) {
+        if (s >= prices.length)
+            return 0;
+        int max = 0;
+        for (int start = s; start < prices.length; start++) {
+            int maxprofit = 0;
+            for (int i = start + 1; i < prices.length; i++) {
+                if (prices[start] < prices[i]) {
+                    int profit = calculate(prices, i + 1) + prices[i] - prices[start];
+                    if (profit > maxprofit)
+                        maxprofit = profit;
+                }
+            }
+            if (maxprofit > max)
+                max = maxprofit;
+        }
+        return max;
+    }
+}
